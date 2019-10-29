bb49e3e45d4a86b7c37aa9a40f3842ef365c3d6c
Kamran Ali <kgali21@gmail.com>
2019-10-28 17:34:49 -0700

    test passing!

--- insertionSort/insertion-sort.js
+++ insertionSort/insertion-sort.js
@@ -1,9 +1,9 @@
 const insertionSort = arr => {
-  for (let i = 0; i < arr.length; i++) {
+  for(let i = 0; i < arr.length; i++) {
     let number = arr[i];
     let j;
 
-    for (j = i - 1; j >= 0 && arr[j] > number; j--) {
+    for(j = i - 1; j >= 0 && arr[j] > number; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = number;
--- insertionSort/insertion-sort.test.js
+++ insertionSort/insertion-sort.test.js
@@ -1,9 +1,9 @@
+const insertionSort = require('./insertion-sort');
+
 describe('last 6 weeks of code challenges', () => {
   it('insertion sort', () => {
     const arr = [4, 1, 3, 6, 2, 5];
     const result = insertionSort(arr);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
-
-
 });
