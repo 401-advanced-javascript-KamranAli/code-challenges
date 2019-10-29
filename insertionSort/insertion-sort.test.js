9b483164aa472dd0d93c2daa8b062b9f22027bdf
Kamran Ali <kgali21@gmail.com>
2019-10-28 17:33:26 -0700

    finished insertion sort test

---
+++ insertionSort/insertion-sort.test.js
@@ -0,0 +1,9 @@
+describe('last 6 weeks of code challenges', () => {
+  it('insertion sort', () => {
+    const arr = [4, 1, 3, 6, 2, 5];
+    const result = insertionSort(arr);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+
+});
