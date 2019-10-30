6d4ef685c4984033fb75800ed67e91e6511d5ad8
Kamran Ali <kgali21@gmail.com>
2019-10-30 07:21:43 -0700

    finished code challenge and tests passing

--- mergeSort/merge-sort.js
+++ mergeSort/merge-sort.js
@@ -1,14 +1,27 @@
 function merge(firstHalf, secondHalf) {
-  const final 
+  const finalArr = [];
+
+  let f = 0;
+  let s = 0;
+  
+  while(f < firstHalf.length && s < secondHalf.length){
+    if(firstHalf[f] < secondHalf[s]){
+      finalArr.push(firstHalf[f++]);
+    } else {
+      finalArr.push(secondHalf[s++]);
+    }
+  }
+
+  return finalArr.concat(firstHalf.slice(f)).concat(secondHalf.slice(s));
 }
 
 function mergeSort(arr) {
   if(arr.length < 2) {
     return arr;
   } else {
-    const mid = parseInt(arr.length / 2);
+    const mid = Math.floor(arr.length / 2);
     const firstHalf = arr.slice(0, mid);
-    const secondHalf = arr.slice(mid, arr.length);
+    const secondHalf = arr.slice(mid);
     return merge(mergeSort(firstHalf), mergeSort(secondHalf));
   }
 }
--- mergeSort/merge-sort.test.js
+++ mergeSort/merge-sort.test.js
@@ -2,9 +2,10 @@ const { merge, mergeSort } = require('./merge-sort');
 
 describe('merge sort', () => {
   it('merge sort strategy for arrays', () => {
-    const arr = [4, 3, 9, 8, 1, 5];
-    const mergeResult = merge(arr);
+    const arr1 = [4, 3, 9];
+    const arr2 = [8, 1, 5];
+    const mergeResult = merge(arr1, arr2);
     const sortResult = mergeSort(mergeResult);
-    expect(sortResult).toEqual();
+    expect(sortResult).toEqual([1, 3, 4, 5, 8, 9]);
   });
 });
