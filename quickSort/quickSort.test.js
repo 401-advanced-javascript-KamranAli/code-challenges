6998e3406941f5d8bee0f44a0821bf465a5184a0
Kamran Ali <kgali21@gmail.com>
2019-10-31 06:59:53 -0700

    retesting to pass travis

--- quickSort/quickSort.test.js
+++ quickSort/quickSort.test.js
@@ -3,7 +3,7 @@ const quicksort = require('./quickSort');
 describe('Quick Sort', () => {
 
   it('should properly quick sort an array', () => {
-    const arr = [4, 3, 5, 1, 6, 9, 400];
+    const arr = [4, 3, 5, 1, 6, 9];
     const left = 0;
     const right = arr.length - 1;
     quicksort(arr, left, right);
