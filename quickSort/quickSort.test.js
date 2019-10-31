a7110cf7ddab82fd903cdd32924890c0dcd3982f
Kamran Ali <kgali21@gmail.com>
2019-10-31 06:57:23 -0700

    quick sort finished and tested fine!

---
+++ quickSort/quickSort.test.js
@@ -0,0 +1,13 @@
+const quicksort = require('./quickSort');
+
+describe('Quick Sort', () => {
+
+  it('should properly quick sort an array', () => {
+    const arr = [4, 3, 5, 1, 6, 9, 400];
+    const left = 0;
+    const right = arr.length - 1;
+    quicksort(arr, left, right);
+    expect(arr).toEqual([1, 3, 4, 5, 6, 9]);
+  });
+
+});
