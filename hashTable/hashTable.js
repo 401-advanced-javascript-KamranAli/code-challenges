76936309bb158b6b4f24954f345492de92ede75c
Kamran Ali <kgali21@gmail.com>
2019-11-04 07:42:59 -0800

    finished tests and challenge

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -3,7 +3,38 @@ class HashTable {
     this.arr = new Array(buckets).fill([]);
   }
 
-  add(key, value)
+  add(key, value) {
+    const index = this.hash(key);
+    const bucket = this.arr[index];
+    bucket.push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.arr[index];
+    const pair = bucket.find(([savedKey]) => savedKey === key);
+    if(pair) {
+      return pair[1];
+    } else {
+      return null;
+    }
+  }
+
+  contains(key) {
+    const boolArr = [];
+    this.arr.forEach(bucket => {
+      bucket.map(keyVal => {
+        boolArr.push(keyVal.includes(key));
+      });
+    });
+    return boolArr.includes(true);
+  }
+
+  hash(key){
+    return key.split('').reduce((acc, letter, i) => 
+      acc += letter.charCodeAt(0) + i, 0)
+      % this.arr.length;
+  }
 
 }
 
--- hashTable/hashTable.test.js
+++ hashTable/hashTable.test.js
@@ -2,9 +2,45 @@ const HashTable = require('./hashTable');
 
 describe('Hash Table', () => {
 
-  it('Adds a hash table', () => {
-    
+  it('Creates a hash table', () => {
+    const hashTab = new HashTable();
+    expect(hashTab.hash('kam')).toBe(0);
   });
 
+  it('adds a key value pair', () => {
+    const hashTab = new HashTable();
+    hashTab.add('kam', 'megan');
+    expect(hashTab.get('kam')).toBe('megan');
+  });
+
+  it('check if a hash exists', () => {
+    const hashTab = new HashTable();
+    hashTab.add('kam', 'anna');
+    expect(hashTab.contains('kam')).toBe(true);
+    expect(hashTab.contains('jared')).toBe(false);
+  });
+
+  it('returns null for a key that doesnt exist', () => {
+    const hashTab = new HashTable();
+    hashTab.add('kam', 'anna');
+    expect(hashTab.get('marty')).toEqual(null);
+  });
+
+  it('returns null for a key that doesnt exist, and returns an existing key', () => {
+    const hashTab = new HashTable();
+    hashTab.add('kam', 'anna');
+    expect(hashTab.get('marty')).toEqual(null);
+    expect(hashTab.get('kam')).toEqual('anna');
+  });
+
+  it('gets a key', () => {
+    const hashTab = new HashTable();
+    hashTab.add('kam', 'megan');
+    hashTab.add('megan', 'jared');
+    hashTab.add('jared', 'kam');
+    expect(hashTab.get('kam')).toEqual('megan');
+    expect(hashTab.get('megan')).toEqual('jared');
+    expect(hashTab.get('jared')).toEqual('kam');
+  });
 
 });
